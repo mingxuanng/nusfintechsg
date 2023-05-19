a043f2462e2c7d14dd78995f4a4e932d262537f0
Ming Xuan <ngmingxuan1995@gmail.com>
2023-05-19 09:56:06 +0800

    Initiated node project

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-hello world
+My first repo
---
+++ recap.js
@@ -0,0 +1,76 @@
+// let x = 2
+// let y = 2
+// let z = x + y
+// let age = ""
+
+function multiply(a, b) {
+    return a*b
+}
+
+result = multiply(2, 3)
+console.log(result)
+
+function addition(a, b) {
+    return a+b
+}
+
+result = addition(2, 3)
+console.log(result)
+
+function checkLeap(a) {
+    if 
+}
+
+result = addition(2, 3)
+console.log(result)
+
+// function checkAge(a) {
+//     let person = ""
+//     if (a >= 18)
+//         person = "Adult"
+//     else
+//         person = "Minor"
+//     return person
+// }
+
+// let person = {
+//     firstname: "Fred",
+//     lastname: "Bloggs",
+//     email: "fred.bloggs@gmail.com" // rmb not to put comma at the end
+// }
+
+// let family = [  // array starts with square bracket
+//     {
+//         firstname: "Fred",
+//         lastname: "Bloggs",
+//         role: "Father"
+//     },
+//     {
+//         firstname: "Mary",
+//         lastname: "Bloggs",
+//         role: "Mother"
+//         phone: "94567584" // ok to have fields that another record don't have
+//     }
+
+// ]
+
+// result = checkAge(z)
+// console.log(result)
+
+/*
+    this is a comment block
+*/ 
+
+// if (z >= 18) {
+//     console.log("18 or over")
+//     age = "Adult"
+// }
+// else {
+//     console.log("18 or less")
+//     age = "Minor"
+//     if (z < 5)
+//         console.log("Small Child")
+// }
+
+// console.log(age)
+// console.log("Value of z: ", z)
